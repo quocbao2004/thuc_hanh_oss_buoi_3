f8f648517649d1696e849ab40721e8d326a603ac
Huynh Nguyen Tan Phat <huynhnguyentanp@gmail.com>
2025-11-01 08:36:56 +0700

    ham kiem tra chan le

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
 int hieu(int a, int b) {
 	return a - b;
-int tich(int a, int b);
+}
 int tong(int a, int b){
 	return a + b;
 }
-int hieu(int a, int b);
 int tich(int a, int b){
 	return a * b ;
 }
@@ -17,6 +15,11 @@ float thuong(int a, int b) {
 	 return -1;
 	}
 	return a/b;
+}
+bool checkChanLe(int n){
+	if(n%2==0)
+		cout<<n<<" la so chan."
+	else cout<<n<<" la so le."
 }
 int main()
 {
